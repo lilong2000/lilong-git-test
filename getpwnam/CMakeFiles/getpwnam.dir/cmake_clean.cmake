d5f236ab998addc07b0b97c3615b3a76224342b6
lilong <lilong_li2000@163.com>
2021-12-06 18:28:18 +0800

    v1.5

---
+++ getpwnam/CMakeFiles/getpwnam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/getpwnam.dir/getpwnam.o"
+  "getpwnam.pdb"
+  "getpwnam"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/getpwnam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sigcld/CMakeFiles/sigcld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sigcld.dir/sigcld.o"
+  "sigcld.pdb"
+  "sigcld"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sigcld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
