880fc87b0dc4c29eb23ceb852847381e86460ea5
lilong <lilong_li2000@163.com>
2021-12-16 17:54:36 +0800

    v2.0

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ v1.6 add alarm longjmp sigalrm sigpending
 v1.7 update yunsuan.sh add catch critical ed sigsetjmp sync
 v1.8 add control exitcode pexit sleep tid
 v1.9 add cleanup
+v2.0 add timedlock barrier timeout
---
+++ timedlock/CMakeFiles/timedlock.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/timedlock.dir/timedlock.c.o"
+  "timedlock.pdb"
+  "timedlock"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/timedlock.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
